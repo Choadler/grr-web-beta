c57a25b638fe8040791e304ae1acd1fb7ee77aef
Choadler <Choadler@users.noreply.github.com>
2026-08-07 00:59:08 -0400

    Load production bundle without module execution

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const preserveModuleScripts = {
-  name: 'preserve-module-scripts-on-cloudflare',
+const preserveScriptsOnCloudflare = {
+  name: 'preserve-scripts-on-cloudflare',
+  renderChunk(code: string) {
+    return code
+      .replaceAll('import.meta.resolve', 'null')
+      .replaceAll('import.meta.url', 'document.baseURI')
+  },
   transformIndexHtml: {
     order: 'post' as const,
     handler(html: string) {
       return html.replace(
-        /<script type="module"/g,
-        '<script data-cfasync="false" type="module"',
+        /<script type="module" crossorigin src=/g,
+        '<script data-cfasync="false" defer src=',
       )
     },
   },
 }
 
-export default defineConfig({ plugins: [react(), preserveModuleScripts] })
+export default defineConfig({
+  plugins: [react(), preserveScriptsOnCloudflare],
+  build: { modulePreload: false },
+})
