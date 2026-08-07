112ee326a93f5031deb97accfba030ebd7a1749f
Choadler <Choadler@users.noreply.github.com>
2026-08-07 12:45:41 -0400

    Refresh production asset revisions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import './styles/global.css'
+
+document.documentElement.dataset.buildRevision = 'gt-behind-leader-1'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
