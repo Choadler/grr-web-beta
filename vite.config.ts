a64848f9071e269b259dcc76ea428b0b7721155e
Choadler <Choadler@users.noreply.github.com>
2026-08-07 01:23:18 -0400

    Fix production asset routing

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ const preserveScriptsOnCloudflare = {
     handler(html: string) {
       return html.replace(
         /<script type="module" crossorigin src="([^"]+)"><\/script>/g,
-        '<script data-cfasync="false" defer src="$1?v=grr-classic-v2"></script>',
+        '<script data-cfasync="false" defer src="$1"></script>',
       )
     },
   },
